01ad88ae7087e327b6bee16685088e6de5b753ad
Joshua Humphrey <jchumphrey2004@gmail.com>
2025-04-14 10:24:32 -0500

    The end is nearing and I'm so happy for it

---
+++ Homework6/BasicDE.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def function1(x,t):
+    return x
+
+def function2(x,t):
+    return x*t
+
+def function3(x,t):
+    result = (np.cos(x))**2 + (np.sin(t))**2
+    return result
