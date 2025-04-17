1433fcabcbea83358ad41b59ee291b8d7a017f2a
Joshua Humphrey <jchumphrey2004@gmail.com>
2025-04-17 09:20:23 -0500

    I hope this works!

---
+++ Homework6/LocalPackage/RK_Solve.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+def fourth_rk(function,start,end,steps):
+    x2 = np.zeros(steps)
+    t2 = np.linspace(start,end,steps)
+    h = (end-start)/steps
+
+    for i in range(1,steps):
+        k1 = h*function(x2[i-1],t2[i-1])
+        k2 = h*function(x2[i-1]+k1/2, t2[i-1] + h/2)
+        k3 = h*function(x2[i-1]+k2/2, t2[i-1] + h/2)
+        k4 = h*function(x2[i-1]+k3, t2[i-1] + h)
+        x2[i] = x2[i-1] + (k1+2*k2+2*k3+k4)/6
+    
+    return x2
+
